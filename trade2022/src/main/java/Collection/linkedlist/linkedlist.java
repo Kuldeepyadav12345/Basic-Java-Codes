fb817f045271fbf22219964527e824b26d52db1d
liveurlife@1234 <Kuldeepyadav12345>
2021-10-25 01:24:39 +0530

    Linked List

--- trade2022/src/main/java/Collection/linkedlist/linkedlist.java
+++ trade2022/src/main/java/Collection/linkedlist/linkedlist.java
@@ -1,5 +1,19 @@
 package Collection.linkedlist;
-
+import java.util.*;
+/*Linked list maintains the insertion order*/
 public class linkedlist {
+	public static void main(String[] args) {
+		LinkedList<String> l=new LinkedList<String>();
+		l.add("kuldeep");
+		l.add("Sam");
+		l.add("Ram");
+		l.add("Sham");
+		System.out.println(l);
+		l.push("Gaurav");
+		System.out.println(l);
+		l.listIterator(0);
+		System.out.println(l.listIterator(0));
+	}
+	
 
 }
